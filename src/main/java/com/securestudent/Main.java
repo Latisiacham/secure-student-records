dda8528db5dce6d7bdfdc35c6c170b72676b4b22
landu <landujhb025@student.wethinkcode.co.za>
2026-09-04 22:33:17 +0200

    chore: set up Java project

---
+++ src/main/java/com/securestudent/Main.java
@@ -0,0 +1,9 @@
+package com.securestudent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=== Secure Student Records ===");
+        System.out.println("Application started successfully.");
+    }
+}
